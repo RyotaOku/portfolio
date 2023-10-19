a3c178a34df7d8af771a71628ec7370e366347e9
RyotaOku <ryota1991o@gmail.com>
2023-10-19 19:57:57 +0900

    actioncreatorに移動

--- src/actions/actioncreator.ts
+++ src/actions/actioncreator.ts
@@ -7,21 +7,21 @@ export async function getAllArchives(dispatch: React.Dispatch<Actions>) {
     dispatch({ type: 'GET_ARCHIVES_REQUEST' });
 
     try {
-        const res = await fetch('/api/portfolio/getAllArchives', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Pragma': 'no-cache',
-                'Cache-Control': 'no-cache',
-            },
-            body: JSON.stringify({}),
-        });
+        // const res = await fetch('/api/portfolio/getAllArchives', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Pragma': 'no-cache',
+        //         'Cache-Control': 'no-cache',
+        //     },
+        //     body: JSON.stringify({}),
+        // });
 
-        if (!res.ok) {
-            throw new Error('Server error');
-        }
+        // if (!res.ok) {
+        //     throw new Error('Server error');
+        // }
 
-        const result = await res.json();
+        // const result = await res.json();
 
         // 成功のアクションを発行
         dispatch({
